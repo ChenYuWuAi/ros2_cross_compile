0c751244642a08d2a2b88e7465aa08b05b17b0ea
程浩 Cheng Hao <chengnangong@163.com>
2024-11-09 18:30:24 +0800

    Update setup_qemu_environment.sh

--- setup_qemu_environment.sh
+++ setup_qemu_environment.sh
@@ -12,7 +12,7 @@ TAG="cpp_pubsub:1.0-arm64"
 if [ "$EUID" -ne 0 ]; then
     sudo echo -e "\033[1;32mGaining root access..."
     if [ $? -ne 0 ]; then
-        echo -e "\031[1;31mFailed to gain root access"
+        echo -e "\033[1;31mFailed to gain root access"
         exit 1
     fi
     echo -e "\033[1;33mRoot access granted\033[0m"
